64aaff87a5cd9193072da6d53435f0f1fde06402
hugo-michel <orayito62@gmail.com>
2024-03-07 18:35:55 +0100

    ajout btn delete fonctionnel

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myLibrary = [
+let myLibrary = [
 	{ title: "The Hobbit", author: "JRR Tolkien", pages: 320, read: true },
 	{ title: "Les fourmis", author: "Bernard Werber", pages: 560, read: false },
 ];
@@ -9,7 +9,7 @@ const READ = document.querySelector("#read");
 const ADDBOOK = document.querySelector("#add-book");
 const FORM = document.querySelector("#form");
 const SHOWBOOK = document.querySelector("#showBook");
-// let BTNDELETE = document.querySelectorAll(".btnDelete");
+let BTNDELETE = document.querySelectorAll(".btnDelete");
 
 function Book(title, author, pages, read) {
 	this.title = title;
@@ -43,7 +43,7 @@ function showBook() {
 
 	myLibrary.forEach((book) => {
 		let newCard = document.createElement("div");
-        newCard.setAttribute("class", "card");
+		newCard.setAttribute("class", "card");
 		let content = `${book.title} by ${book.author}, ${book.pages} pages, read : ${book.read}`;
 
 		let newP = document.createElement("p");
@@ -52,25 +52,29 @@ function showBook() {
 		let btnDelete = document.createElement("button");
 		btnDelete.setAttribute("class", "btnDelete");
 		btnDelete.textContent = "X";
- 
+
 		newCard.appendChild(newP);
 		newCard.appendChild(btnDelete);
 
 		SHOWBOOK.appendChild(newCard);
+		BTNDELETE = document.querySelectorAll(".btnDelete");
+	});
+}
 
-        let BTNDELETE = document.querySelectorAll(".btnDelete");
-        BTNDELETE.forEach(btn => {
-            btn.addEventListener("click", () => {
-                btn.parentElement.remove();
-            })
-        })
-
+function deleteBook() {
+	BTNDELETE.forEach((btn, index) => {
+		btn.addEventListener("click", () => {
+			btn.parentElement.remove();
+			myLibrary.splice(index, 1);
+			BTNDELETE = document.querySelectorAll(".btnDelete");
+		});
 	});
 }
 
 ADDBOOK.addEventListener("click", () => {
 	addBookToLibrary();
 	showBook();
+	deleteBook();
 });
 
-window.onload = showBook();
+(window.onload = showBook()), deleteBook();
