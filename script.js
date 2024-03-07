624b944e28a4b71c7be01fca610a44f1e0eb9d4a
hugo-michel <orayito62@gmail.com>
2024-03-07 18:05:12 +0100

    ajout btn delete entrée visuellement

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const myLibrary = [
-    {title: "The Hobbit", author: "JRR Tolkien", pages: 320, read: true},
-    {title: "Les fourmis", author: "Bernard Werber", pages: 560, read: false},
+	{ title: "The Hobbit", author: "JRR Tolkien", pages: 320, read: true },
+	{ title: "Les fourmis", author: "Bernard Werber", pages: 560, read: false },
 ];
 const TITLE = document.querySelector("#title");
 const AUTHOR = document.querySelector("#author");
@@ -9,6 +9,7 @@ const READ = document.querySelector("#read");
 const ADDBOOK = document.querySelector("#add-book");
 const FORM = document.querySelector("#form");
 const SHOWBOOK = document.querySelector("#showBook");
+// let BTNDELETE = document.querySelectorAll(".btnDelete");
 
 function Book(title, author, pages, read) {
 	this.title = title;
@@ -36,22 +37,40 @@ function addBookToLibrary() {
 }
 
 function showBook() {
+	while (SHOWBOOK.firstChild) {
+		SHOWBOOK.firstChild.remove();
+	}
 
-    while (SHOWBOOK.firstChild) {
-        SHOWBOOK.firstChild.remove()
-    }
+	myLibrary.forEach((book) => {
+		let newCard = document.createElement("div");
+        newCard.setAttribute("class", "card");
+		let content = `${book.title} by ${book.author}, ${book.pages} pages, read : ${book.read}`;
 
-    myLibrary.forEach(book => {
-        let content = `${book.title} by ${book.author}, ${book.pages} pages, read : ${book.read}`;
-        let newP = document.createElement("p");
-        newP.textContent = content;
-        SHOWBOOK.appendChild(newP);
-    })
+		let newP = document.createElement("p");
+		newP.textContent = content;
+
+		let btnDelete = document.createElement("button");
+		btnDelete.setAttribute("class", "btnDelete");
+		btnDelete.textContent = "X";
+ 
+		newCard.appendChild(newP);
+		newCard.appendChild(btnDelete);
+
+		SHOWBOOK.appendChild(newCard);
+
+        let BTNDELETE = document.querySelectorAll(".btnDelete");
+        BTNDELETE.forEach(btn => {
+            btn.addEventListener("click", () => {
+                btn.parentElement.remove();
+            })
+        })
+
+	});
 }
 
 ADDBOOK.addEventListener("click", () => {
-    addBookToLibrary();
-    showBook();
+	addBookToLibrary();
+	showBook();
 });
 
 window.onload = showBook();
