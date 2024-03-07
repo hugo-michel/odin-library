efd9faf592d645141b293a550f9ba14d88170229
hugo-michel <orayito62@gmail.com>
2024-03-07 17:33:29 +0100

    affichage liste livre

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-const myLibrary = [];
+const myLibrary = [
+    {title: "The Hobbit", author: "JRR Tolkien", pages: 320, read: true},
+    {title: "Les fourmis", author: "Bernard Werber", pages: 560, read: false},
+];
 const TITLE = document.querySelector("#title");
 const AUTHOR = document.querySelector("#author");
 const PAGES = document.querySelector("#pages");
 const READ = document.querySelector("#read");
 const ADDBOOK = document.querySelector("#add-book");
 const FORM = document.querySelector("#form");
+const SHOWBOOK = document.querySelector("#showBook");
 
 function Book(title, author, pages, read) {
 	this.title = title;
@@ -31,4 +35,23 @@ function addBookToLibrary() {
 	READ.checked = false;
 }
 
-ADDBOOK.addEventListener("click", addBookToLibrary);
+function showBook() {
+
+    while (SHOWBOOK.firstChild) {
+        SHOWBOOK.firstChild.remove()
+    }
+
+    myLibrary.forEach(book => {
+        let content = `${book.title} by ${book.author}, ${book.pages} pages, read : ${book.read}`;
+        let newP = document.createElement("p");
+        newP.textContent = content;
+        SHOWBOOK.appendChild(newP);
+    })
+}
+
+ADDBOOK.addEventListener("click", () => {
+    addBookToLibrary();
+    showBook();
+});
+
+window.onload = showBook();
